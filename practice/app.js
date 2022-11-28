a663d772f65c6d105b6d912936c12a6be0cff6f4
streamradix <111672276+streamradix@users.noreply.github.com>
2022-11-29 04:00:02 +0900

    temp2

--- practice/app.js
+++ practice/app.js
@@ -1,14 +1,13 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-	const userName = loginInput.value;
-
-	if (userName === "") {
-		alert("Please write your name");
-	} else if (userName.length > 15) {
-		alert("Too Long name");
-	}
+function onLoginBtnClick(event) {
+	event.preventDefault(); // 이벤트 기본동작 막기
+	console.log(event);
+	//const username = loginInput.value;
+	//console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+//loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginBtnClick);
