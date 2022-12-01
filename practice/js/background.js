9e3e32633ceeaf21fb49d8e9a7b06a6fe23ce4d6
streamradix <111672276+streamradix@users.noreply.github.com>
2022-12-01 20:25:13 +0900

    check for test5

--- practice/js/background.js
+++ practice/js/background.js
@@ -5,5 +5,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
-console.log(bgImage);
+//document.body.appendChild(bgImage);
+//console.log(bgImage);
+
+document.body.style.background = `linear-gradient(${Math.floor(Math.random() * 360)}deg, red, yellow)`;
